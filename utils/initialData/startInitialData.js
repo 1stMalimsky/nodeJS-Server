af3917a1d7908b6f739ead580620e0cd37161308
alonMalimsky <alonmalichi@gmail.com>
2023-06-18 11:18:22 +0300

    initial data fixed

--- utils/initialData/startInitialData.js
+++ utils/initialData/startInitialData.js
@@ -7,7 +7,6 @@ const hashServiceModel = require("../../utils/hash/hashService");
 const normalizeUser = require("../../utils/normalize/normalizeUser");
 const normalizeCard = require("../../utils/normalize/normalizeCard");
 
-
 const checkData = async () => {
     try {
         let allUsers = await userServiceModel.getAllUsers();
@@ -15,22 +14,27 @@ const checkData = async () => {
         if (allUsers.length === 0) {
             await Promise.all(
                 userList.map(async (user) => {
-                    normalizeUser(user);
-                    user.password = await hashServiceModel.generateHash(user.password);
-                    await userServiceModel.registerUser(user);
+                    try {
+                        normalizeUser(user);
+                        user.password = await hashServiceModel.generateHash(user.password);
+                        await userServiceModel.registerUser(user);
+                    }
+                    catch (err) {
+                        res.status(500).json({ message: err.message || err });
+                    }
                 })
-            );
+            )
             console.log("Users created");
         }
         allUsers = await userServiceModel.getAllUsers()
-        if (allCards.length == 0 && allUsers.length > 0) {
+        if (allCards.length == 0) {
             for (let i = 0; i < 3; i++) {
                 normalizeCard(cardList[i]);
                 await cardsServiceModel.createCard({ ...cardList[i], user_id: allUsers[i]._id });
             };
-            console.log("cards created");
+            console.log("Cards Created");
         }
-        else return console.log("data exists");
+        else return console.log("Database already exists");
     }
     catch (err) {
         console.log(chalk.red.bold("initial err: ", err.message || err));
